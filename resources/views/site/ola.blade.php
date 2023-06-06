bf783a8c3af593f98282f6566d9befa3a968276e
arthursenais <arthur_sa@estudante.sesisenai.org.br>
2023-06-06 11:49:36 -0300

    Update ola.blade.php

--- resources/views/site/ola.blade.php
+++ resources/views/site/ola.blade.php
@@ -1,10 +1,24 @@
 @extends('layout.master')
-@section('title','Bem-vindo!')
-<nav class="green">
+@section('title', 'Bem-vindo!')
+
+<nav style="background-color: #8FADB2;">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Biblioteca do kauê</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><input type="text"></li>
-      </ul>
+        <a href="#" class="brand-logo"><img src="">logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li class="active"><a href="#">Catálogo</a></li>
+            <li><a href="#">Mais vendidos</a></li>
+            <li><a href="#">Recem adicionados</a></li>
+            <li><a href="#" class="text-black">Indicações</a></li>
+            <li><i class="material-icons">search</i><input type="search" id="search"></li>
+        </ul>
+
+
+
+
+
+
+
+
+
     </div>
-  </nav>
+</nav>
