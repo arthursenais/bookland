378346d0f2c544043584fc919166cfc6d6041c19
Arthur Sá <arthur_sa@estudante.sesisenai.org.br>
2023-06-11 18:28:34 -0300

    Pequenas Mudanças

--- resources/views/site/details.blade.php
+++ resources/views/site/details.blade.php
@@ -16,10 +16,9 @@
             <a href="" class="text-gray-600 hover:text-gray-950 hover:underline transition-all">{{$livro->categoria->nome}}</a>
             <p>{{$livro->descricao}}</p>
 
-           <div class="flex">
+           <div class="flex sm:mt-10">
             <button class="sm:bg-inherit border-l  border-t border-b border-sky-600 hover:bg-sky-600 hover:text-white bg-sky-600 sm:p-0 sm:px-2 p-2 sm:text-sky-600 text-white sm:rounded-l-full rounded  transition">Empréstimo</button>
             <button class="sm:bg-inherit border-r  border-t border-b border-red-500 hover:bg-red-500 hover:text-white bg-red-500 sm:p-0 sm:px-2 p-2 sm:text-red-500 text-white sm:rounded-r-full rounded transition">Reserva</button>
-
            </div>
         </div>
     </div>
